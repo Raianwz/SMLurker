4dffe78bf0a4bbc1f9a18b754847fd0b12cfad66
Raianwz <raian03@live.com>
2023-01-31 00:09:52 -0300

    Visual transitions moved to new file

--- preload.js
+++ preload.js
@@ -2,8 +2,10 @@
 const { contextBridge } = require('electron')
 const Eremote = require('@electron/remote')
 const pfs = require('fs')
+const ppath = require('path')
 const { WControls } = require('./src/internal/controls')
 const { SCore } = require('./src/internal/smcore')
+const path = require('path')
 
 WControls(Eremote)
 
@@ -35,6 +37,26 @@ const api = {
         rd: (path) => pfs.readFileSync(path, { encoding: 'utf8' }),
         write: (path, args) => pfs.writeFileSync(path, args),
     },
+    path: {
+        fjoin: (args) => path.join(__filename, args),
+        fresolve: (args) => path.resolve(__filenamem, args),
+        join: (args) => path.join(__dirname, args),
+        resolve: (args) => path.resolve(__dirname, args),
+    },
+    dg: {
+        showMB: (opts) => Eremote.dialog.showMessageBoxSync(opts),
+    },
+    tr: {
+        changeside: (btn, dest) => {
+            let { changeSides } = require('./src/components/window/transitions')
+            changeSides(btn, dest)
+        },
+        blockinput: (value) => {
+            let { blockInputs } = require('./src/components/window/transitions')
+            blockInputs(value)
+        }
+    }
+
 }
 
 
